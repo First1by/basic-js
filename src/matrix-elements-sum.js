f6377909aafb4b2a6941759aa6f6eccc2075bf6f
First1by <greysound1991@mail.ru>
2022-10-17 22:53:45 +0300

    feat: done tasks

--- src/matrix-elements-sum.js
+++ src/matrix-elements-sum.js
@@ -30,6 +30,20 @@ function getMatrixElementsSum(matrix) {
 		return arr.flat().reduce((sum, current) => sum + current, 0);
 }
 
+// function getMatrixElementsSum(matrix) {
+//   let sum = 0;
+//   for (let j = 0; j < matrix[0].length; j++) {
+//     for (let i = 0; i < matrix.length; i++) {
+//       if (matrix[i][j] !== 0) {
+//         sum += matrix[i][j];
+//       } else {
+//         break;
+//       }
+//     }
+//   }
+//   return sum;
+// }
+
 module.exports = {
   getMatrixElementsSum
 };
